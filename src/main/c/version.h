b91dd1d9bf59ec8663402c21ba5a624fd5521fae
Richard Maxwell <richard@rsmaxwell.co.uk>
2022-05-20 14:47:02 +0000

    initial version

--- src/main/c/version.h
+++
@@ -1,9 +0,0 @@
-
-
-#define VERSION      "<VERSION>"
-#define BUILD_ID     "<BUILD_ID>"
-#define BUILD_DATE   "<BUILD_DATE>"
-#define GIT_COMMIT   "<GIT_COMMIT>"
-#define GIT_BRANCH   "<GIT_BRANCH>"
-#define GIT_URL      "<GIT_URL>"
-
---
+++ src/main/go/go.mod
@@ -0,0 +1,3 @@
+module github.com/rsmaxwell/job-to-xml
+
+go 1.17
