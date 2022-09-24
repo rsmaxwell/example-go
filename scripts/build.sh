5ab5526a0751568cd1ee2ca97ef9a4334a17815e
Richard Maxwell <richard@rsmaxwell.co.uk>
2022-09-24 16:52:46 +0100

    debug

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,10 @@
 
 set -x
 
+set | grep GO
+
+echo 
+
 BUILD_DIR=../build
 GOOS=$(go env GOOS)
 GOARCH=$(go env GOENV)
@@ -12,5 +16,3 @@ mkdir -p ${BUILD_DIR}
 
 
 go build -o ${BUILD_DIR}/${PROJECT_NAME}-${GOOS}-${GOARCH}${GOEXE} ./...
-
-'tree $WORKSPACE'
