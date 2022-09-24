5d5d1839f1b5e82a2cd6173a445f0b3eead1fb7c
Richard Maxwell <richard@rsmaxwell.co.uk>
2022-09-24 22:16:09 +0100

    reorg

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
+set -x
 
 NAME=example-go
 ZIPFILE=${NAME}_amd64-linux.zip
-
 
 BUILD_DIR=./build
 PACKAGE_DIR=./package
 DIST_DIR=./dist
 
-rm -rf ${PACKAGE_DIR}
+rm -rf ${PACKAGE_DIR} ${DIST_DIR}
 mkdir -p ${PACKAGE_DIR} ${DIST_DIR}
 
 cd ${PACKAGE_DIR}
