00b772fe3b1ff8a5cbd041df7db0ac1988b6a660
Richard Maxwell <richard@rsmaxwell.co.uk>
2022-09-25 09:40:10 +0100

    reorg

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,6 +4,17 @@ set -x
 
 BUILD_DIR=./build
 
+go get -u github.com/gorilla/mux
+
 go build -o ${BUILD_DIR}/ ./...
 
-ls -al ${BUILD_DIR}
+ls -al /go
+ls -al /go/bin
+ls -al /go/pkg
+ls -al /go/src
+
+pwd
+echo "GOROOT = $GOROOT"
+echo "GOPATH = $GOPATH"
+
+ls -al $BUILD_DIR}
