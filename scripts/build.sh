142b2e3aabe61f759d164dd5103f7d695a056231
Richard Maxwell <richard@rsmaxwell.co.uk>
2022-09-25 13:23:26 +0100

    reorg

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,24 +2,8 @@
 
 set -x 
 
-pwd
-
-echo "GOROOT = $(go env GOROOT)"
-echo "GOPATH = $(go env GOPATH)"
-
-echo "Initial contents of $GOPATH"
-find $GOPATH
-
 BUILD_DIR=./build
 
 go get -u github.com/gorilla/mux
 
 go build -o ${BUILD_DIR}/ ./...
-
-echo "Final contents of $GOPATH"
-find $GOPATH
-
-ls -al $BUILD_DIR
-
-echo "Contents of ${BUILD_DIR}"
-find $BUILD_DIR
